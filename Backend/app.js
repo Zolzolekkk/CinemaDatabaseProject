c83b7faad1b360454ce7e6409cd8df70098ce166
Zolzolekkk <115138342+Zolzolekkk@users.noreply.github.com>
2023-05-07 22:42:41 +0200

    Probydalej

--- Backend/app.js
+++ Backend/app.js
@@ -8,43 +8,36 @@ import { MongoClient } from 'mongodb';
 /* *************************** */
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 app.locals.pretty = app.get('env') === 'development'; // The resulting HTML code will be indented in the development environment
 /* ************************************************ */
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public')); 
+app.use(morgan('dev')); 
+app.use(express.json());
 /* ******** */
 /* "Routes" */
 /* ******** */
 app.get('/', async function (request, response) {
-    // const MongoCLient = require('mongodb').MongoClient;
-    // const client = new MongoClient('mongodb://127.0.0.1:27017');
-    // await client.connect();
-    // const db = client.db('AGH');
-    // const collection = db.collection('students');
-    // const students = await collection.find({}).toArray();
-    response.render('index');
+    const client = new MongoClient('mongodb+srv://bazydanych2023agh:mongodb123@cinema.xo2lkmi.mongodb.net/');
+    await client.connect();
+    const db = client.db('test');
+    const collection = db.collection('films');
+    const movies = await collection.find({}).toArray();
+
+    
+    response.json(movies);
 });
-app.get('/submit', function (request, response) {
-    const name = request.query.name;
-  });
 
-app.post('/', function (request, response) {
-    let body = '';
 
-        request.on('data', (chunk) => {
-          body += chunk.toString();
-        });
+
+app.post('/', async function (request, response) {
+      const client = new MongoClient('mongodb+srv://bazydanych2023agh:mongodb123@cinema.xo2lkmi.mongodb.net/');
+      await client.connect();
+      const db = client.db('test');
+      const collection = db.collection('films');
+      const movies = await collection.find({}).toArray();
+      console.log(movies);
 
         request.on('end', () => {
-          console.log(body);
-          const formData = new URLSearchParams(body);
-
-          const name = formData.get('name');
-          response.render("gh");
-          response.end();
+          response.json(movies);
         });
 });
 /* ************************************************ */
@@ -52,5 +45,3 @@ app.listen(8000, function () {
     console.log('The server was started on port 8000');
     console.log('To stop the server, press "CTRL + C"');
 });
-
-// module.exports = app;
